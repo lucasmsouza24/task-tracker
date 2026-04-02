7407cd1f43d907ab9efc8bddd71bdfe1c2a15120
Lucas Mesquita de Souza <lucasmesquita58@hotmail.com>
2026-04-02 13:24:18 -0300

    feat: :sparkles: implement CLI commands structure for task management (add, list, remove)

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
-def main():
-    print("Hello from task-tracker!")
+#!/usr/bin/env python3
 
+import sys
+
+# commands
+def add(args):
+    print('ADD called with:', args)
+    
+def list_tasks(args):
+    print('LIST called')
+
+def remove(args):
+    print('REMOVE called with args:', args)
+    
+COMMANDS = {
+    'add': add,
+    'list': list_tasks,
+    'remove': remove,
+}
+
+# CLI entrypoint
+def main():
+    args = sys.argv[1:]
+    
+    if not args:
+        print('Usage: task-cli <command> [args]')
+        print('Commands:', ', '.join(COMMANDS.keys()))
+
+    command = args[0]
+    command_args = args[1:]
+    
+    func = COMMANDS.get(command)
+    
+    if not func:
+        print('Unknow comand: {command}')
+        print('Available commands: ', ', '.join(COMMANDS.keys()))
+        return
+    
+    func(command_args)
+    
 if __name__ == "__main__":
     main()
