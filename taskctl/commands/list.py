eb1b506cb11527466e1d419b56779be1c4780897
Lucas Mesquita de Souza <lucasmesquita58@hotmail.com>
2026-04-02 23:22:11 -0300

    feat: implement task listing functionality in list command

--- taskctl/commands/list.py
+++ taskctl/commands/list.py
@@ -1,2 +1,15 @@
+from taskctl.core.json_storage import JSONStorage
+
 def run(args):
-    print('LIST called')
+    storage = JSONStorage()
+    
+    list_values = storage.read()
+    
+    print('-' * 40)
+    for task in list_values:
+        print(f'id: {task["id"]}')
+        print(f'{task["description"]}')
+        print(f'status: {task["status"]}')
+        print(f'created_at: {task["created_at"]}')
+        print(f'updated_at: {task["updated_at"]}')
+        print('-' * 40)
